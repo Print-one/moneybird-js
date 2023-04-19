f15841ec5acde11b448efe0354b288bc40457925
UnderKoen <koenvanstaveren@hotmail.com>
2023-04-19 15:24:38 +0200

    fix: export Adminstration, Contact and SalesInvoice in index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Moneybird } from "./moneybird";
 
 export { Moneybird };
+export { Administration } from "./administration";
+export { Contact } from "./contact";
+export { SalesInvoice } from "./salesInvoice";
 export { MoneybirdOptions } from "./common";
 
 export const instance: Moneybird = new Moneybird(
