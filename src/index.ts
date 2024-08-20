d1f0e08b8df5c891259af3b0daebc7794371bdd3
UnderKoen <koenvanstaveren@hotmail.com>
2024-08-20 09:44:04 +0200

    fix: export TimeoutError

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export { Administration } from "./administration";
 export { Contact } from "./contact";
 export { SalesInvoice } from "./salesInvoice";
 export * from "./common";
+export * from "./errors";
 
 export const instance: Moneybird = new Moneybird(
   "https://moneybird.com/api/",
